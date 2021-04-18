3708af6b20633cac6f43ebce003cf2648f5e7b51
Nairu Guan <guan.nairu@gmail.com>
2021-04-17 20:00:33 -0400

    added favicon

--- car/urls.py
+++ car/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
 from .views import (index, register, DashboardView, 
             add_car, del_car, profile, car_view, edit_car, add_vin, 
             add_record, remove_record, get_record_info, 
             edit_record, testdata, warranty_data)
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -24,4 +26,9 @@ urlpatterns = [
     path('my-cars/get-service-info/<int:pk>', get_record_info),
     path('my-cars/service/edit/<int:pk>', edit_record, name='edit-record'),
     path('test-data/<str:vin>', testdata), 
+
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
